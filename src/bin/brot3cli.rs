69f6fec15eb74838628f00a2e7c26f31cbef0166
Ross Younger <crazyscot@gmail.com>
2024-01-17 16:06:54 +1300

    tidyup cli help metadata

--- src/bin/brot3cli.rs
+++ src/bin/brot3cli.rs
@@ -11,32 +11,33 @@ use std::error::Error;
 #[command(author, version, about, long_about = None)]
 #[command(disable_help_flag = true)]
 struct Cli {
-    /// Output filename (required; use '-' for stdout)
-    #[arg(short = 'o', long = "output")]
+    /// Where to send the output (required; use '-' for stdout)
+    #[arg(short = 'o', long = "output", value_name = "FILENAME")]
     output_filename: String,
 
     // TODO: fractal: Option<String>, - defaulted
     /// Rendering type
-    #[arg(short, long)]
+    #[arg(short, long, value_name = "NAME")]
     renderer: Option<WhichRenderer>,
 
     // TODO: plot params
     // optional one of origin, centre <complex float> - default from fractal
     // optional one of axis-length, pixel-length, zoom-factor <float OR complex float> - default from fractal - a float f => (f,f)
-    #[arg(short, long, default_value = "512")]
+    /// Maximum number of iterations
+    #[arg(short, long, value_name = "N", default_value = "512")]
     max_iter: u32,
 
     /// Plot width
-    #[arg(short, long, default_value = "300")]
+    #[arg(short, long, value_name = "PIXELS", default_value = "300")]
     width: usize,
     /// Plot height
-    #[arg(short, long, default_value = "300")]
+    #[arg(short, long, value_name = "PIXELS", default_value = "300")]
     height: usize,
 
     /// Enable debug output (may be repeated)
     #[arg(short, long, action = clap::ArgAction::Count, help = "Enables debug output (may be repeated)")]
     debug: u8,
-    /// Maximum
+
     /// This help message
     #[arg(long="help", action = ArgAction::Help)]
     help: Option<bool>,
