efe9c70dbe1a4e57e50e5b9a15c9c1a515e96846
Ross Younger <crazyscot@gmail.com>
2024-01-18 22:15:37 +1300

    png: refactor render into two

--- src/render/png.rs
+++ src/render/png.rs
@@ -21,47 +21,54 @@ impl Png {
     }
 }
 
+impl Png {
+    fn render_inner(
+        &self,
+        tile: &Tile,
+        writer: Box<dyn std::io::Write>,
+    ) -> Result<(), Box<dyn Error>> {
+        let mut encoder = png::Encoder::new(writer, tile.spec.width, tile.spec.height);
+        encoder.set_color(png::ColorType::Rgba);
+        encoder.set_depth(png::BitDepth::Eight);
+
+        encoder.add_text_chunk("software".to_string(), "brot3".to_string())?;
+        encoder.add_text_chunk("comment".to_string(), tile.info_string())?;
+
+        // MAYBE: allow user to specify gamma of their monitor?
+        encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));
+        // MAYBE: set source chromaticities?
+
+        let mut png_writer = encoder.write_header()?;
+        let mut image_data =
+            Vec::<u8>::with_capacity(4 * tile.spec.width as usize * tile.spec.height as usize);
+        let tile_data = tile.result();
+        let max_iter = tile.max_iter_plotted;
+        tile_data
+            .elements_row_major_iter()
+            .map(|pd| {
+                if pd.iter == max_iter {
+                    f64::INFINITY
+                } else {
+                    pd.iterations()
+                }
+            })
+            .for_each(|iters| {
+                let col = colour(iters);
+                for b in col {
+                    image_data.push(b);
+                }
+            });
+        png_writer.write_image_data(&image_data)?;
+        Ok(())
+    }
+}
+
 impl Renderer for Png {
     fn render(&self, tile: &Tile) -> Result<(), Box<dyn Error>> {
         let path = Path::new(self.filename.get());
         let file = File::create(path)?;
-        let mut w = &mut BufWriter::new(file);
-        {
-            let mut encoder = png::Encoder::new(&mut w, tile.spec.width, tile.spec.height);
-            encoder.set_color(png::ColorType::Rgba);
-            encoder.set_depth(png::BitDepth::Eight);
-
-            encoder.add_text_chunk("software".to_string(), "brot3".to_string())?;
-            encoder.add_text_chunk("comment".to_string(), tile.info_string())?;
-
-            // MAYBE: allow user to specify gamma of their monitor?
-            encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));
-            // MAYBE: set source chromaticities?
-
-            let mut writer = encoder.write_header()?;
-            let mut image_data =
-                Vec::<u8>::with_capacity(4 * tile.spec.width as usize * tile.spec.height as usize);
-            let tile_data = tile.result();
-            let max_iter = tile.max_iter_plotted;
-            tile_data
-                .elements_row_major_iter()
-                .map(|pd| {
-                    if pd.iter == max_iter {
-                        f64::INFINITY
-                    } else {
-                        pd.iterations()
-                    }
-                })
-                .for_each(|iters| {
-                    let col = colour(iters);
-                    for b in col {
-                        image_data.push(b);
-                    }
-                });
-            writer.write_image_data(&image_data)?;
-        }
-
-        w.flush()?;
+        let w = Box::new(BufWriter::new(file)) as Box<dyn Write>;
+        self.render_inner(tile, w)?;
         Ok(())
     }
 }
