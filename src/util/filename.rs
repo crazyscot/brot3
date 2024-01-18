2304c2240c7ae4fc8746a81ac2bb802f1e217825
Ross Younger <crazyscot@gmail.com>
2024-01-18 22:50:58 +1300

    filename: write_handle gives a buffered handle

--- src/util/filename.rs
+++ src/util/filename.rs
@@ -2,7 +2,7 @@
 // (c) 2024 Ross Younger
 
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufWriter, Write};
 use std::path::Path;
 
 #[derive(PartialEq)]
@@ -19,13 +19,17 @@ impl Filename {
     pub fn get(&self) -> &String {
         &self.filename
     }
+    /// Truncatingly opens the given file for writing and returns a buffered write handle.
+    /// You should call flush() before dropping the handle.
     pub fn write_handle(&self) -> std::io::Result<Box<dyn Write>> {
         if self.filename == "-" {
+            // stdout is buffered already
             Ok(Box::new(std::io::stdout()))
         } else {
             let path = Path::new(&self.filename);
             let file = File::create(path)?;
-            Ok(Box::new(file))
+            let bw = Box::new(BufWriter::new(file)) as Box<dyn Write>;
+            Ok(bw)
         }
     }
 }
