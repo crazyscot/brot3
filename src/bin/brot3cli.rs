19461bc9bc53caeacae7bcf499ced56ef0fc228e
Ross Younger <crazyscot@gmail.com>
2024-01-19 13:29:48 +1300

    cli: move styling out to a module

--- src/bin/brot3cli.rs
+++ src/bin/brot3cli.rs
@@ -1,6 +1,7 @@
 // brot3 command line interface
 // (c) 2024 Ross Younger
 use brot3::{
+    cli::styles,
     fractal::{
         userplotspec::{UserPlotLocation, UserPlotSize},
         PlotSpec, Point, Scalar, Tile, UserPlotSpec,
@@ -14,7 +15,7 @@ use clap::{ArgAction, Args, Parser, Subcommand};
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(disable_help_flag = true)]
-#[command(styles=get_styles())]
+#[command(styles=styles::get_styles())]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
@@ -183,42 +184,4 @@ fn list(args: ListArgs) -> anyhow::Result<()> {
         }
     }
     Ok(())
-}
-
-pub fn get_styles() -> clap::builder::Styles {
-    clap::builder::Styles::styled()
-        .usage(
-            anstyle::Style::new()
-                .bold()
-                .underline()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
-        )
-        .header(
-            anstyle::Style::new()
-                .bold()
-                .underline()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
-        )
-        .literal(
-            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
-        )
-        .invalid(
-            anstyle::Style::new()
-                .bold()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
-        )
-        .error(
-            anstyle::Style::new()
-                .bold()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
-        )
-        .valid(
-            anstyle::Style::new()
-                .bold()
-                .underline()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
-        )
-        .placeholder(
-            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::White))),
-        )
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![warn(missing_debug_implementations)]
 
+pub mod cli;
 pub mod fractal;
 pub mod render;
 pub mod util;
