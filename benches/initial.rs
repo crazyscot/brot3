60fda07f6e81634dc3d94f62bbda4a6d1d879d37
Ross Younger <crazyscot@gmail.com>
2024-01-19 20:07:18 +1300

    benchmark mandelbrot_pixel

--- benches/initial.rs
+++ benches/initial.rs
@@ -1,16 +1,19 @@
+use brot3::fractal::{mandelbrot_pixel, mandelbrot_prepare, Point, PointData};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n - 1) + fibonacci(n - 2),
-    }
+/// A point (found by experiment) that's in the set but not in the special-case cut-off regions
+const TEST_POINT: Point = Point::new(-0.158_653_6, 1.034_804);
+
+fn pixel(c: &mut Criterion) {
+    let template = PointData::new(TEST_POINT);
+    c.bench_function("mandelbrot_pixel", |b| {
+        let mut point = template;
+        mandelbrot_prepare(&mut point);
+        b.iter(|| mandelbrot_pixel(&mut point, black_box(16384)))
+    });
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-}
+// also tile, colour
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, pixel);
 criterion_main!(benches);
