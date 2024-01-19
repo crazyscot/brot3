5fa1ba4e46d55eda5bf995003a3120d670f70e5d
Ross Younger <crazyscot@gmail.com>
2024-01-19 20:18:20 +1300

    benchmark tiles

--- benches/initial.rs
+++ benches/initial.rs
@@ -1,4 +1,4 @@
-use brot3::fractal::{mandelbrot_pixel, mandelbrot_prepare, Point, PointData};
+use brot3::fractal::{mandelbrot_pixel, mandelbrot_prepare, PlotSpec, Point, PointData, Tile};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 /// A point (found by experiment) that's in the set but not in the special-case cut-off regions
@@ -9,11 +9,26 @@ fn pixel(c: &mut Criterion) {
     c.bench_function("mandelbrot_pixel", |b| {
         let mut point = template;
         mandelbrot_prepare(&mut point);
-        b.iter(|| mandelbrot_pixel(&mut point, black_box(16384)))
+        b.iter(|| mandelbrot_pixel(&mut point, black_box(512)))
     });
 }
 
-// also tile, colour
+const TEST_TILE_SPEC: PlotSpec = PlotSpec {
+    origin: Point { re: -1.0, im: 0.0 },
+    axes: Point { re: 4.0, im: 4.0 },
+    width: 300,
+    height: 300,
+};
 
-criterion_group!(benches, pixel);
+fn tile(c: &mut Criterion) {
+    c.bench_function("mandelbrot_tile", |b| {
+        let mut tile = Tile::new(&TEST_TILE_SPEC, 0);
+        tile.prepare();
+        b.iter(|| {
+            tile.plot(black_box(512));
+        });
+    });
+}
+
+criterion_group!(benches, pixel, tile);
 criterion_main!(benches);
