b867242083acaf6e6a08873e9644c01440243bf4
Ross Younger <crazyscot@gmail.com>
2024-01-20 22:55:28 +1300

    add mandel3 to benchmark, make benchmark groups work

--- benches/initial.rs
+++ benches/initial.rs
@@ -4,16 +4,31 @@ use brot3::render::{self, Renderer, SelectionRDiscriminants};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 /// A point (found by experiment) that's in the set but not in the special-case cut-off regions
-const TEST_POINT: Point = Point::new(-0.158_653_6, 1.034_804);
+const TEST_POINT_M2: Point = Point::new(-0.158_653_6, 1.034_804);
+const TEST_POINT_M3: Point = Point::new(-0.573_133_7, 0.569_299_8);
 
-fn pixel(c: &mut Criterion) {
-    let template = PointData::new(TEST_POINT);
-    let alg = fractal::factory(SelectionFDiscriminants::Original);
-    c.bench_function("mandelbrot_pixel", |b| {
-        let mut point = template;
-        alg.prepare(&mut point);
-        b.iter(|| alg.pixel(black_box(&mut point), black_box(512)))
-    });
+// TODO: We should be able to macrify or otherwise de-duplicate this across the algorithms. Perhaps a table of (algorithm, point).
+fn pixels(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fractals");
+
+    {
+        let template = PointData::new(TEST_POINT_M2);
+        let alg = fractal::factory(SelectionFDiscriminants::Original);
+        group.bench_function("mandelbrot_pixel", |b| {
+            let mut point = template;
+            alg.prepare(&mut point);
+            b.iter(|| alg.pixel(black_box(&mut point), black_box(512)))
+        });
+    }
+    {
+        let template = PointData::new(TEST_POINT_M3);
+        let alg = fractal::factory(SelectionFDiscriminants::Mandel3);
+        group.bench_function("mandelbrot3_pixel", |b| {
+            let mut point = template;
+            alg.prepare(&mut point);
+            b.iter(|| alg.pixel(black_box(&mut point), black_box(512)))
+        });
+    }
 }
 
 const TEST_TILE_SPEC: TileSpec = TileSpec {
@@ -24,7 +39,8 @@ const TEST_TILE_SPEC: TileSpec = TileSpec {
 };
 
 fn tile(c: &mut Criterion) {
-    c.bench_function("mandelbrot_tile", |b| {
+    let mut group = c.benchmark_group("tiles");
+    group.bench_function("mandelbrot_tile", |b| {
         let alg = fractal::factory(SelectionFDiscriminants::Original);
         let mut tile = Tile::new(&TEST_TILE_SPEC, &alg, 0);
         b.iter(|| {
@@ -46,14 +62,19 @@ const PALETTE_TILE_SPEC: TileSpec = TileSpec {
     height: 300,
 };
 
+// TODO: We should be able to macrify this for additional palettes.
+// Could we in fact autobench all the palettes?
 fn colour_pixel(c: &mut Criterion) {
-    c.bench_function("colour_pixel_mandy", |b| {
+    let mut group = c.benchmark_group("palettes");
+
+    group.bench_function("colour_pixel_mandy", |b| {
         b.iter(|| render::colour_temp(black_box(42.0)));
     });
 }
 
 fn colour_tile(c: &mut Criterion) {
-    c.bench_function("colour_tile_mandy", |b| {
+    let mut group = c.benchmark_group("palettes");
+    group.bench_function("colour_tile_mandy", |b| {
         let alg = fractal::factory(SelectionFDiscriminants::Original);
         let mut tile = Tile::new(&PALETTE_TILE_SPEC, &alg, 0);
         tile.plot(384);
@@ -62,5 +83,6 @@ fn colour_tile(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, pixel, tile, colour_pixel, colour_tile);
-criterion_main!(benches);
+criterion_group!(benches, pixels, tile);
+criterion_group!(palettes, colour_pixel, colour_tile);
+criterion_main!(benches, palettes);
