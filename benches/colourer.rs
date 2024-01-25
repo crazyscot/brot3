22b7678f57869bba0a210e871c479274634a2808
Ross Younger <crazyscot@gmail.com>
2024-01-25 22:22:58 +1300

    overhaul palette benchmarker, rename to colourer

---
+++ benches/colourer.rs
@@ -0,0 +1,20 @@
+use brot3::colouring::{self, OutputsRgb8, PaletteInstance, Selection};
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use strum::IntoEnumIterator;
+
+fn colour_pixel(c: &mut Criterion) {
+    let mut group = c.benchmark_group("colourers");
+    let mut bench = |sel: Selection| {
+        let instance = colouring::factory(sel);
+        group.bench_function(format!("{sel:?}"), |b| {
+            b.iter(|| {
+                instance.colour_rgb8(black_box(42.0));
+            });
+        });
+    };
+    PaletteInstance::iter().for_each(|i| bench(i.into()));
+}
+
+criterion_group!(colourers, colour_pixel);
+criterion_main!(colourers);
