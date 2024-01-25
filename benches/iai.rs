3afe450934fb66102381050dacad9cdeaefa1ed9
Ross Younger <crazyscot@gmail.com>
2024-01-25 22:02:56 +1300

    rework iai plot benchmark, include m3

--- benches/iai.rs
+++ benches/iai.rs
@@ -8,30 +8,33 @@ use brot3::{
 
 /// A point (found by experiment) that's in the set but not in the special-case cut-off regions
 const TEST_POINT_M2: Point = Point::new(-0.158_653_6, 1.034_804);
-//const TEST_POINT_M3: Point = Point::new(-0.573_133_7, 0.569_299_8);
+const TEST_POINT_M3: Point = Point::new(-0.573_133_7, 0.569_299_8);
 
 struct BenchData {
     point: PointData,
     fractal: FractalInstance,
 }
 
-fn setup_iteration() -> BenchData {
-    let mut point = PointData::new(TEST_POINT_M2);
-    let fractal = fractal::factory(fractal::Selection::Original);
+use fractal::Selection::*;
+
+fn setup_iteration(point_to_use: Point, alg: fractal::Selection) -> BenchData {
+    let mut point = PointData::new(point_to_use);
+    let fractal = fractal::factory(alg);
     fractal.prepare(black_box(&mut point));
     BenchData { point, fractal }
 }
 
 #[library_benchmark]
-#[bench::short(setup_iteration())]
-fn bench_iteration_m2(mut bd: BenchData) -> PointData {
+#[bench::m2(setup_iteration(TEST_POINT_M2, Original))]
+#[bench::m3(setup_iteration(TEST_POINT_M3, Mandel3))]
+fn bench_iteration(mut bd: BenchData) -> PointData {
     bd.fractal.iterate(&mut bd.point);
     bd.point
 }
 
 library_benchmark_group!(
-    name = bench_iter_group;
-    benchmarks = bench_iteration_m2
+    name = iteration;
+    benchmarks = bench_iteration
 );
 
 ///////////////////////////////////////////////////////////////////////////
@@ -51,4 +54,4 @@ library_benchmark_group!(
     benchmarks = bench_colourer,
 );
 
-main!(library_benchmark_groups = bench_iter_group, colourer);
+main!(library_benchmark_groups = iteration, colourer);
