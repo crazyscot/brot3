36aa1b7fc2d54fb11d5d43d93f527756f3846e17
Ross Younger <crazyscot@gmail.com>
2024-01-27 20:36:44 +1300

    cli: print the info string

--- src/cli/plot.rs
+++ src/cli/plot.rs
@@ -260,6 +260,7 @@ pub fn plot(args: &Args, debug: u8) -> anyhow::Result<()> {
     if args.no_split {
         let mut t = Tile::new(&spec, debug);
         t.plot(args.max_iter);
+        println!("{}", t.info_string());
         renderer.render(&t)
     } else {
         let time0 = SystemTime::now();
@@ -268,10 +269,10 @@ pub fn plot(args: &Args, debug: u8) -> anyhow::Result<()> {
         let time1 = SystemTime::now();
         tiles.par_iter_mut().for_each(|t| t.plot(args.max_iter));
         let time2 = SystemTime::now();
-        let result = Tile::join(&spec, &tiles)?;
+        let tile = Tile::join(&spec, &tiles)?;
         let time3 = SystemTime::now();
 
-        let res = renderer.render(&result);
+        let result = renderer.render(&tile);
         let time4 = SystemTime::now();
         if args.show_timing {
             println!(
@@ -282,7 +283,8 @@ pub fn plot(args: &Args, debug: u8) -> anyhow::Result<()> {
                 time4.duration_since(time3).unwrap_or_default(),
             );
         }
-        res
+        println!("{}", tile.info_string());
+        result
     }
 }
 
