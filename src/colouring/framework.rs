6f5ea73a532de1bbd3cdd1c39a633e171ab9851a
Ross Younger <crazyscot@gmail.com>
2024-01-30 16:14:34 +1300

    convert colourer to use enum_delegate

--- src/colouring/framework.rs
+++ src/colouring/framework.rs
@@ -1,35 +1,35 @@
 // Palette selection & dispatch framework
 // (c) 2024 Ross Younger
 
-use enum_dispatch::enum_dispatch;
-use strum_macros::{Display, EnumDiscriminants, EnumIter, EnumMessage, EnumString};
+use enum_delegate;
+use strum_macros::{Display, EnumDiscriminants, EnumIter, EnumMessage, EnumString, FromRepr};
 
 use super::direct_rgb::Mandy;
-use super::huecycles::{self, LinearRainbow};
+use super::huecycles::LinearRainbow;
 use super::types::White;
-use super::{direct_rgb, Rgb8};
+use super::Rgb8;
 
 /// Selector for available Palettes
-#[enum_dispatch]
-#[derive(Clone, Copy, Debug, Display, EnumIter, EnumMessage, PartialEq)]
+#[enum_delegate::implement(OutputsRgb8)]
+#[derive(Clone, Copy, Debug, Display, EnumIter, EnumMessage, FromRepr, PartialEq)]
 #[strum(serialize_all = "kebab_case")]
 #[derive(EnumDiscriminants)] // This creates the enum Selection ...
 #[strum_discriminants(name(Selection), derive(clap::ValueEnum, EnumIter, EnumString))] // ... and specifies what it derives from
 #[allow(clippy::module_name_repetitions)] // enum_dispatch doesn't support structs with the same name but different paths
 pub enum ColourerInstance {
     /// A continuous cycle around the HSV cone with fixed saturation and lightness
-    LinearRainbow,
+    LinearRainbow(LinearRainbow),
 
     /// The colouring algorithm from ``mandy`` by rjk
-    Mandy,
+    Mandy(Mandy),
 
     /// Test algorithm that always outputs white pixels
     #[strum(disabled)]
-    White,
+    White(White),
 }
 
 /// A colouring algorithm that outputs Rgb8 directly.
-#[enum_dispatch(ColourerInstance)]
+#[enum_delegate::register]
 pub trait OutputsRgb8 {
     /// Colouring function
     fn colour_rgb8(&self, iters: f64) -> Rgb8;
@@ -37,12 +37,10 @@ pub trait OutputsRgb8 {
 
 /// Factory method
 #[must_use]
+#[allow(clippy::missing_panics_doc)]
 pub fn factory(selection: Selection) -> ColourerInstance {
-    match selection {
-        Selection::LinearRainbow => huecycles::LinearRainbow {}.into(),
-        Selection::Mandy => direct_rgb::Mandy {}.into(),
-        Selection::White => White {}.into(),
-    }
+    ColourerInstance::from_repr(selection as usize)
+        .expect("Failed to convert enum discriminant into instance (can't happen)")
 }
 
 #[cfg(test)]
