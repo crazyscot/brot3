2b193e2a836a2d79da8a5776d2ed2056702b3350
Ross Younger <crazyscot@gmail.com>
2024-01-30 16:02:16 +1300

    listable: add list2() for now

--- src/util/listable.rs
+++ src/util/listable.rs
@@ -1,7 +1,9 @@
 // Common support code for enums which the user can select from
 // (c) 2024 Ross Younger
 
-use strum::{EnumMessage, IntoEnumIterator};
+use std::str::FromStr;
+
+use strum::{EnumMessage, IntoEnumIterator, VariantNames};
 
 /// Returns a list of all available items for a given type
 #[must_use]
@@ -26,6 +28,36 @@ pub fn list<T: IntoEnumIterator + std::fmt::Display + EnumMessage>(machine_parse
                 r.get_documentation().unwrap_or_default(),
                 width = longest
             );
+        })
+        .collect::<Vec<_>>();
+}
+
+/// Prints a list of available items for a given type
+pub fn list2<T: VariantNames + FromStr + std::fmt::Display + EnumMessage>(machine_parseable: bool) {
+    let v = &T::VARIANTS;
+
+    if machine_parseable {
+        println!("{v:?}");
+        return;
+    }
+
+    let longest = v.iter().map(|r| r.len()).max().unwrap_or(1);
+
+    let _ = v
+        .iter()
+        .map(|name| {
+            // Due to an issue with EnumIter that appeared in strum 0.26.1,
+            // we iterate over the names, turn them back into enum members (sigh!),
+            // then query the enum member `val` for its docstring.
+            let res = T::from_str(name);
+            if let Ok(val) = res {
+                println!(
+                    "  {:width$}  {}",
+                    name,
+                    val.get_documentation().unwrap_or_default(),
+                    width = longest
+                );
+            }
         })
         .collect::<Vec<_>>();
 }
