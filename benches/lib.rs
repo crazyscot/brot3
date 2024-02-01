a985b1c83da0b53706adb80b711d4eb3ba9d7bb1
Ross Younger <crazyscot@gmail.com>
2024-02-01 21:30:34 +1300

    combine colourer and fractal benches into one (to reduce linker work)

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,6 +1,10 @@
+// brot3 library benchmarking
+// (c) 2024 Ross Youinger
+
+use brot3::colouring::{self, Instance, OutputsRgb8, Selection::*};
 use brot3::fractal::{self, Algorithm, Point, PointData};
 
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 
 /// A point (found by experiment) that's in the set but not in the special-case cut-off regions
 const TEST_POINT_M2: Point = Point::new(-0.158_653_6, 1.034_804);
@@ -28,5 +32,23 @@ fn iteration(c: &mut Criterion) {
     alg(fractal::Selection::Variant, TEST_POINT_M3);
 }
 
+fn colour_pixel(c: &mut Criterion) {
+    let mut group = c.benchmark_group("colourers");
+    let mut bench = |instance: Instance| {
+        group.bench_function(format!("{}", instance), |b| {
+            b.iter(|| {
+                instance.colour_rgb8(black_box(42.0), 256);
+            });
+        });
+    };
+    // We run only a selection of algorithms through the full benchmarker
+    // (See also IAI, which runs them all.)
+    let selection = [LinearRainbow, LchGradient, Mandy, WhiteFade];
+    selection
+        .iter()
+        .for_each(|i| bench(colouring::Instance::from_repr(*i as usize).unwrap()));
+}
+
 criterion_group!(fractals, iteration);
-criterion_main!(fractals);
+criterion_group!(colourers, colour_pixel);
+criterion_main!(fractals, colourers);
