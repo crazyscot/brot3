a579d4bc7f3071665e4f4f243ba469034efacbec
Ross Younger <crazyscot@gmail.com>
2024-02-02 16:15:09 +1300

    remove unnecessary collect()

--- src/util/listable.rs
+++ src/util/listable.rs
@@ -6,9 +6,8 @@ use std::str::FromStr;
 use strum::{EnumMessage, IntoEnumIterator, VariantNames};
 
 /// Returns a list of all available items for a given type
-#[must_use]
-pub fn list_vec<T: IntoEnumIterator + std::fmt::Display>() -> Vec<String> {
-    T::iter().map(|a| a.to_string()).collect()
+pub fn list_vec<T: IntoEnumIterator + std::fmt::Display>() -> impl Iterator<Item = String> {
+    T::iter().map(|a| a.to_string())
 }
 
 /// Prints a list of available items for a given type
@@ -22,21 +21,18 @@ pub fn list<T: VariantNames + FromStr + std::fmt::Display + EnumMessage>(machine
 
     let longest = v.iter().map(|r| r.len()).max().unwrap_or(1);
 
-    let _ = v
-        .iter()
-        .map(|name| {
-            // Due to an issue with EnumIter that appeared in strum 0.26.1,
-            // we iterate over the names, turn them back into enum members (sigh!),
-            // then query the enum member `val` for its docstring.
-            let res = T::from_str(name);
-            if let Ok(val) = res {
-                println!(
-                    "  {:width$}  {}",
-                    name,
-                    val.get_documentation().unwrap_or_default(),
-                    width = longest
-                );
-            }
-        })
-        .collect::<Vec<_>>();
+    v.iter().for_each(|name| {
+        // Due to an issue with EnumIter that appeared in strum 0.26.1,
+        // we iterate over the names, turn them back into enum members (sigh!),
+        // then query the enum member `val` for its docstring.
+        let res = T::from_str(name);
+        if let Ok(val) = res {
+            println!(
+                "  {:width$}  {}",
+                name,
+                val.get_documentation().unwrap_or_default(),
+                width = longest
+            );
+        }
+    });
 }
