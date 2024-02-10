f4dbc5c41c6e8ebd6991fd93b279a6aba165b90a
Ross Younger <crazyscot@gmail.com>
2024-02-10 17:07:05 +1300

    dynamically size & resize viewer to fill the window

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,10 +1,21 @@
 import './style.css'
 import { invoke } from '@tauri-apps/api'
 import OpenSeadragon from 'openseadragon'
+import $ from 'jquery'
 
 document.querySelector<HTMLDivElement>('#main')!.innerHTML = `
-  <div id="seadragon-viewer" style="width: ${window.innerWidth}px; height: ${window.innerHeight}px;"></div>
-`
+<div id="seadragon-viewer"></div>
+`;
+// Dynamically size to fill the window. This is also used to resize.
+let viewerElement = $('#seadragon-viewer');
+if (viewerElement.height() == 0) {
+  viewerElement.height(window.innerHeight);
+  console.log(`H set to ${viewerElement.height()}`)
+}
+if (viewerElement.width() == 0) {
+  viewerElement.width(window.innerWidth);
+  console.log(`W set to ${viewerElement.width()}`)
+}
 
 // now we can call our Command!
 // Right-click the application background and open the developer tools.
@@ -31,5 +42,23 @@ var viewer = OpenSeadragon({
   id: "seadragon-viewer",
   prefixUrl: "//openseadragon.github.io/openseadragon/images/",
   tileSources: duomo,
-  homeFillsViewer: true
+  homeFillsViewer: true,
+  autoResize: true,
+  preserveImageSizeOnResize:true,
+  visibilityRatio: 1.0,
 });
+
+// Rather than caning the system as we get a resize event for every pixel, add a slight debounce
+let redrawer: number|undefined = undefined;
+
+window.addEventListener('resize', function (_event) {
+  if (redrawer !== undefined) {
+    this.clearTimeout(redrawer);
+  }
+  redrawer = setTimeout(function () {
+    console.log(`resizing to ${window.innerWidth} x ${window.innerHeight}`);
+    viewerElement.height(window.innerHeight);
+    viewerElement.width(window.innerWidth);
+    viewer.viewport.applyConstraints();
+  }, 100);
+}, true);
