80263d1f4cf6b1a803a189e0a7ed145e1746a93c
Ross Younger <crazyscot@gmail.com>
2024-02-10 15:28:29 +1300

    tidy up example paraphernalia

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,21 +1,10 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 import { invoke } from '@tauri-apps/api'
 import OpenSeadragon from 'openseadragon'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
   <div id="seadragon-viewer" style="width: 400px; height: 400px;"></div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
 // now we can call our Command!
 // Right-click the application background and open the developer tools.
