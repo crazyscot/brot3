1d140e43379cb59f3877e07f5637df49901b410b
Ross Younger <crazyscot@gmail.com>
2024-02-11 19:44:26 +1300

    refactor Png to add render_rgba(), expose in module

--- engine/src/render.rs
+++ engine/src/render.rs
@@ -6,3 +6,13 @@ mod png;
 
 #[allow(clippy::module_name_repetitions)]
 pub use framework::{factory, RenderInstance, Renderer, Selection};
+
+use crate::colouring::Instance;
+use crate::fractal::Tile;
+
+/// Renders a tile as a low-level array of RGBA values.
+/// These are returned in the obvious left to right, top to bottom order.
+#[must_use]
+pub fn as_rgba(tile: &Tile, colourer: Instance) -> Vec<u8> {
+    png::Png::render_rgba(tile, colourer)
+}
--- engine/src/render/png.rs
+++ engine/src/render/png.rs
@@ -14,24 +14,9 @@ use std::io::BufWriter;
 pub struct Png {}
 
 impl Png {
-    fn render_inner(
-        tile: &Tile,
-        colourer: Instance,
-        writer: Box<dyn std::io::Write>,
-    ) -> Result<()> {
-        let mut encoder = png::Encoder::new(writer, tile.spec.width(), tile.spec.height());
-        encoder.set_color(png::ColorType::Rgba);
-        encoder.set_depth(png::BitDepth::Eight);
-
-        encoder.add_text_chunk("software".to_string(), "brot3".to_string())?;
-        let info = tile.info_string(&colourer);
-        encoder.add_text_chunk("comment".to_string(), info)?;
-
-        // MAYBE: allow user to specify gamma of their monitor?
-        encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));
-        // MAYBE: set source chromaticities?
-
-        let mut png_writer = encoder.write_header()?;
+    /// Renders a tile as a low-level array of RGBA values.
+    /// These are returned in the obvious left to right, top to bottom order.
+    pub(crate) fn render_rgba(tile: &Tile, colourer: Instance) -> Vec<u8> {
         let mut image_data =
             Vec::<u8>::with_capacity(4 * tile.spec.width() as usize * tile.spec.height() as usize);
         let tile_data = tile.result();
@@ -57,6 +42,24 @@ impl Png {
                 image_data.push(b);
                 image_data.push(255);
             });
+        image_data
+    }
+
+    fn render_png(tile: &Tile, colourer: Instance, writer: Box<dyn std::io::Write>) -> Result<()> {
+        let mut encoder = png::Encoder::new(writer, tile.spec.width(), tile.spec.height());
+        encoder.set_color(png::ColorType::Rgba);
+        encoder.set_depth(png::BitDepth::Eight);
+
+        encoder.add_text_chunk("software".to_string(), "brot3".to_string())?;
+        let info = tile.info_string(&colourer);
+        encoder.add_text_chunk("comment".to_string(), info)?;
+
+        // MAYBE: allow user to specify gamma of their monitor?
+        encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));
+        // MAYBE: set source chromaticities?
+
+        let mut png_writer = encoder.write_header()?;
+        let image_data = Self::render_rgba(tile, colourer);
         png_writer.write_image_data(&image_data)?;
         Ok(())
     }
@@ -66,7 +69,7 @@ impl Renderer for Png {
     fn render_file(&self, filename: &str, tile: &Tile, colourer: Instance) -> anyhow::Result<()> {
         let handle = Filename::open_for_writing(filename)?;
         let bw = Box::new(BufWriter::new(handle));
-        Png::render_inner(tile, colourer, bw).with_context(|| "Failed to render PNG")?;
+        Png::render_png(tile, colourer, bw).with_context(|| "Failed to render PNG")?;
         // You can test this error pathway by trying to write to /dev/full
         Ok(())
     }
