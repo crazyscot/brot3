66993079188298f2b7ab96a9da3b93f76a09ecd5
Ross Younger <crazyscot@gmail.com>
2024-02-19 12:24:19 +1300

    add a simple mutexed ts class for allocating serial numbers

---
+++ ui/src/serial_allocator.ts
@@ -0,0 +1,17 @@
+import { Mutex } from 'async-mutex'
+
+export class SerialAllocator {
+    private _next: number;
+    private _mutex: Mutex;
+    constructor() {
+        this._mutex = new Mutex();
+        this._next = 1;
+    }
+    async next() {
+        return await this._mutex.runExclusive(() => {
+            var rv = this._next;
+            if (++this._next == Number.MAX_SAFE_INTEGER) { this._next = 0; }
+            return rv;
+        });
+    }
+}
