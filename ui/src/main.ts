0334527fde11c83f12dcbe29fae64a3a7a41da12
Ross Younger <crazyscot@gmail.com>
2024-03-31 12:34:42 +1300

    [ui] Add close X controls to HUD and position entry form

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -23,10 +23,10 @@ ${About.html}
   <div class="info" id="info-panel">
   <form id="form_go_to_position">
     <table>
-      <tr class="position-display"><th>Origin:</th><td><span id="originReal"></span></td><td><span id="originImag"></span> i</td></tr>
+      <tr class="position-display"><td rowspan="3"><span class="close" id="close-hud">&times;</span></td> <th>Origin:</th><td><span id="originReal"></span></td><td><span id="originImag"></span> i</td></tr>
       <tr class="position-display"><th>Centre:</th><td><span id="centreReal"></span></td><td><span id="centreImag"></span> i</td></tr>
       <tr class="position-display"><th>Axes:</th><td><span id="axesReal"></span></td><td><span id="axesImag"></span> i</td><th>Zoom:</th><td id="zoom"></td></tr>
-      <tr class="position-entry"><th>Origin:</th><td><input type="text" id="enter_originReal" /></td><td>+ <input type="text" id="enter_originImag" /> i</td><td colspan="3"/></tr>
+      <tr class="position-entry"><td rowspan="3"><span class="close" id="close-entry">&times;</span></td> <th>Origin:</th><td><input type="text" id="enter_originReal" /></td><td>+ <input type="text" id="enter_originImag" /> i</td><td colspan="3"/></tr>
       <tr class="position-entry"><th>Axes:</th><td><input type="text" id="enter_axisReal" /> real</td><td><em>or</em> <input type="text" id="enter_axisImag" /> im</td>
         <td><em>or</em> Zoom:</td><td><input type="text" id="enter_zoom" /></td><td><input type="submit" id="action_go_to_position" value="Go"></td></tr>
       <tr class="position-entry"><td colspan="6" id="position-error-text" /></tr>
