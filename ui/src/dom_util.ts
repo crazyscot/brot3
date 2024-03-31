893dcd2f223d5b9fa8cfadc8d1ff5840243dc036
Ross Younger <crazyscot@gmail.com>
2024-03-31 17:31:29 +1300

    [refactor] move DOM utility classes out to separate file

---
+++ ui/src/dom_util.ts
@@ -0,0 +1,32 @@
+// utility code affecting the Document Object Model
+// (c) 2024 Ross Younger
+
+export function tr_is_visible(e: HTMLElement): boolean {
+  return e.style.display !== "none";
+}
+
+// Toggles visibility of a TR element, returning true iff it is now visible.
+export function toggle_tr_visibility(e: HTMLElement) : boolean {
+  if (e.style.display === "none") {
+      e.style.display = "table-row";
+      return true;
+  } else {
+      e.style.display = "none";
+      return false;
+  }
+}
+
+export class ClickEventListener implements EventListenerObject {
+    clickable: Element;
+    action: Function;
+    constructor(target: Element, action: Function) {
+      this.clickable = target;
+      this.clickable.addEventListener("click", this);
+      this.action = action;
+    }
+    handleEvent(event: Event): void | Promise<void> {
+      if (event.type === "click") {
+          this.action(event);
+      }
+    }
+}
