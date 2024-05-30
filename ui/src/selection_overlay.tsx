03af82676272457bbee0be0d5742f6680b44677c
Ross Younger <crazyscot@gmail.com>
2024-05-30 20:02:03 +1200

    Properly recut select modal as a React component

--- ui/src/selection_overlay.tsx
+++ ui/src/selection_overlay.tsx
@@ -1,59 +1,60 @@
 // brot3 fractal/colourer selection
 // (c) 2024 Ross Younger
 
-import { element_is_displayed, toggle_element_visibility } from './dom_util'
 import { createRoot, Root } from 'react-dom/client';
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { effectModalClickOrEscape } from './modal-react';
+import { invoke } from '@tauri-apps/api';
+import { listen } from '@tauri-apps/api/event';
+import { ListItem, ListItemWithKey, add_keys_to_list } from './engine_types';
+import { DisplayMessageDetail } from './menu';
 
-const SelectionModal = ({ doHide = () => { } }) => {
+
+
+const SelectionModal = () => {
+    const [show, setShow] = useState(false);
     const hide = () => {
-        doHide();
+        setShow(false);
     };
-
     const ref = effectModalClickOrEscape(() => {
         hide();
     });
-    return (
-        <div ref={ref}>
-            <div className="modal-content">
-                <span className="close" id="close-selector" onClick={hide}>&times;</span>
-                <h3>Select Fractal</h3>
-                <h1>TODO - populate list</h1>
-            </div>
+
+    useEffect(() => {
+        listen<DisplayMessageDetail>('select', (event) => {
+            let id = event.payload.what;
+            switch (id) {
+                case "fractal":
+                    setShow(true);
+                    break;
+                default:
+                    console.error(`unknown select message detail ${id}`);
+            }
+        });
+    }, []);
+
+    return <>{show && <div className="reactmodal">
+        <div className="modal-content" ref={ref}>
+            <span className="close" id="close-selector" onClick={hide}>&times;</span>
+            <h3>Select Fractal</h3>
+            List data goes here
         </div>
-    );
+    </div>}</>
 };
 
 export class SelectionOverlay {
     // Our base html, which is a React root
-    static readonly html: string = `<div class="selector modal" id="selector-panel"></div>`;
+    static readonly html: string = `<div id="selector-panel"></div>`;
 
     panel: HTMLElement | null;
     readonly root: Root;
 
     constructor(doc: Document) {
-        let self = this; // For closures
+        //let self = this; // For closures
         this.panel = doc.querySelectorAll('#selector-panel')[0] as HTMLElement;
 
-        // Hide by default
-        self.set_visibility(false);
         this.root = createRoot(this.panel);
-        this.root.render(<SelectionModal doHide={() => { this.set_visibility(false) }} />);
-    }
-
-    set_visibility(visible: boolean) {
-        if (visible !== element_is_displayed(this.panel!)) {
-            toggle_element_visibility(this.panel!);
-        }
-    }
-
-    do_select_fractal() {
-        this.set_visibility(true);
-        /* TODO:
-         * Populate a list of JS objects ... this is a React Component, I think ... see React "Your First Component" & "Rendering Lists" help. 
-         * Action on selection
-         */
+        this.root.render(<SelectionModal />);
     }
 
     noop() { }
