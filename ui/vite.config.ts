3a1d7e432e7c5022b663e6f0cc3ea21ab9e38303
Ross Younger <crazyscot@gmail.com>
2024-06-17 19:38:18 +1200

    chore: add vite react plugin to silence a load of noise at build-time

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
@@ -26,6 +27,7 @@ export default defineConfig({
           dest: 'openseadragon/images'
         }
       ]
-    })
+    }),
+    react()
   ]
 })
