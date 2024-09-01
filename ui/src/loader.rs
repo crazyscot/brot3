b1975294a3ee013b6c6cf981ae23719d54e35b6f
Ross Younger <crazyscot@gmail.com>
2024-09-01 18:33:46 +1200

    chore: refactor tile loading out into its own type & module

---
+++ ui/src/loader.rs
@@ -0,0 +1,32 @@
+// Tile loading logic, parallelisation and synchronisation
+// (c) 2024 Ross Younger
+
+use crate::engine;
+use crate::types::TileCoordinate;
+
+use core::future::Future;
+use core::pin::Pin;
+
+use slint::{Rgba8Pixel, SharedPixelBuffer};
+
+pub(crate) struct LoadingTile {
+    pub image: Pin<Box<dyn Future<Output = slint::Image>>>,
+}
+
+impl LoadingTile {
+    pub(crate) fn new(coord: TileCoordinate) -> LoadingTile {
+        LoadingTile {
+            image: Box::pin(async move {
+                let (image_send, image_recv) = tokio::sync::oneshot::channel();
+                rayon::spawn(move || {
+                    let _ = image_send.send(engine::draw_tile(&coord));
+                });
+                let buffer = image_recv.await.unwrap().unwrap_or_else(|e| {
+                    eprintln!("error drawing tile: {e}");
+                    SharedPixelBuffer::<Rgba8Pixel>::new(1, 1)
+                });
+                slint::Image::from_rgba8(buffer)
+            }),
+        }
+    }
+}
