ec3c396bee1f5f4c3d427cbc27a801560154fdf3
Ross Younger <crazyscot@gmail.com>
2024-09-05 21:52:00 +1200

    feat(ui): Function menu and About box

---
+++ ui/src/menu.rs
@@ -0,0 +1,25 @@
+// Menu definition and bindings
+// (c) 2024 Ross Younger
+
+use crate::State;
+use slint::{ComponentHandle, SharedString};
+
+pub(crate) fn handle_menu(state: &std::rc::Weak<State>, what: &SharedString) {
+    match what.as_str() {
+        "About" => do_about(state),
+        _ => println!("Unhandled menu event {what} !!"),
+    }
+}
+
+fn do_about(state: &std::rc::Weak<State>) {
+    let state = state.upgrade().unwrap();
+    let ui = state.main_ui.as_weak();
+    let _ = ui
+        .upgrade_in_event_loop(|u| {
+            u.invoke_about_box();
+        })
+        .map_err(|err| {
+            eprintln!("error: {err}");
+            err
+        });
+}
