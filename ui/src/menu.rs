fc8c5841d852ea8d41613a7d96c7cce7c8109613
Ross Younger <crazyscot@gmail.com>
2024-09-06 20:12:31 +1200

    feat(ui): menu option to show/hide info panel

--- ui/src/menu.rs
+++ ui/src/menu.rs
@@ -7,6 +7,7 @@ use slint::{ComponentHandle, SharedString};
 pub(crate) fn handle_menu(state: &std::rc::Weak<State>, what: &SharedString) {
     match what.as_str() {
         "About" => do_about(state),
+        "toggle-info" => toggle_info(state),
         _ => println!("Unhandled menu event {what} !!"),
     }
 }
@@ -22,4 +23,10 @@ fn do_about(state: &std::rc::Weak<State>) {
             eprintln!("error: {err}");
             err
         });
+}
+
+fn toggle_info(state: &std::rc::Weak<State>) {
+    let state = state.upgrade().unwrap();
+    let ui = &state.main_ui;
+    ui.set_info_visible(!ui.get_info_visible());
 }
