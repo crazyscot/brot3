945664f1747e0f637f222a72731bcbf78b54645f
Ross Younger <crazyscot@gmail.com>
2024-09-08 10:49:37 +1200

    style: use vivi for the menu button

--- ui/build.rs
+++ ui/build.rs
@@ -3,5 +3,9 @@
 
 fn main() {
     // Caution: You can only usefully compile one slint file at the present time. https://github.com/slint-ui/slint/issues/3217
-    slint_build::compile("ui/mainui.slint").unwrap();
+    slint_build::compile_with_config(
+        "ui/mainui.slint",
+        slint_build::CompilerConfiguration::new().with_library_paths(vivi_ui::import_paths()),
+    )
+    .unwrap();
 }
