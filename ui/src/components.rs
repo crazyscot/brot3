d6927d8f327f838541b0e7e0bf5f77d302fc1026
Ross Younger <crazyscot@gmail.com>
2025-04-22 09:56:23 +1200

    chore(deps): remove toolchain pin to 1.80, update slint and lint overrides

--- ui/src/components.rs
+++ ui/src/components.rs
@@ -1,10 +1,8 @@
 // Disable lints on slint-generated code
 #![allow(missing_debug_implementations)]
-#![allow(missing_docs)]
 #![allow(trivial_casts)]
 #![allow(trivial_numeric_casts)]
 #![allow(unreachable_pub)]
-#![allow(clippy::pedantic)]
-#![allow(clippy::missing_errors_doc)]
+#![allow(unused_results)]
 
 slint::include_modules!();
