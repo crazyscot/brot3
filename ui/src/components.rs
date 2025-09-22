f15016f4e3c61eeff735dae5b8b8b95f13b05f8c
Ross Younger <crazyscot@gmail.com>
2025-09-23 00:06:00 +1200

    chore(skip): branch prep: remove ui crate, switch off cli crate for now

--- ui/src/components.rs
+++
@@ -1,8 +0,0 @@
-// Disable lints on slint-generated code
-#![allow(missing_debug_implementations)]
-#![allow(trivial_casts)]
-#![allow(trivial_numeric_casts)]
-#![allow(unreachable_pub)]
-#![allow(unused_results)]
-
-slint::include_modules!();
