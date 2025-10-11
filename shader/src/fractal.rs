crazyscot/brot3

commit 9a47d480cfb4ad59650b8eca85b7de8b4f0d96bb

feat: exponent can be a float

shader/src/fractal.rs
@@ -29,6 +29,8 @@ pub(super) fn render(constants: &FragmentConstants, point: Vec2) -> RenderData {
 pub(crate) trait Fractal: private::Modifier {
     fn iterate(&self, constants: &FragmentConstants) -> FractalResult;
     fn iterate_inner(&self, c: Complex, constants: &FragmentConstants) -> FractalResult {
+        use shader_common::NumericType;
+
         const ESCAPE_THRESHOLD_SQ: f32 = 4.0;
 
         let mut iters = 0;
@@ -46,21 +48,29 @@ pub(crate) trait Fractal: private::Modifier {
         let inside = iters == max_iter && (norm_sqr < ESCAPE_THRESHOLD_SQ);
 
         // Fractional escape count: See http://linas.org/art-gallery/escape/escape.html
-        if constants.exponent_i < 4 {
+
+        let (exponent, ln_exponent) = match constants.exponent.typ {
+            NumericType::Integer if constants.exponent.int == 2 => (2., core::f32::consts::LN_2),
+            NumericType::Integer => (
+                constants.exponent.int as f32,
+                (constants.exponent.int as f32).abs().ln(),
+            ),
+            NumericType::Float => (
+                constants.exponent.float,
+                constants.exponent.float.abs().ln(),
+            ),
+        };
+
+        if exponent.abs() < 4. {
             // A couple of extra iterations helps decrease the size of the error term
-            // This does not work above exp=3, as the repeated iterations quickly cause overflow
-            for _ in 0..2 {
-                z = self.iterate_algorithm(constants, z, c, iters);
-                iters += 1;
-            }
+            // This does not work above about exp=3, as the repeated iterations quickly cause overflow
+            z = self.iterate_algorithm(constants, z, c, iters);
+            iters += 1;
+            z = self.iterate_algorithm(constants, z, c, iters);
+            iters += 1;
         }
         // by the logarithm of a power law,
         // z.norm().ln().ln() === (z.norm_sqr().ln() * 0.5).ln())
-        let ln_exponent = if constants.exponent_i == 2 {
-            core::f32::consts::LN_2
-        } else {
-            (constants.exponent_i as f32).abs().ln()
-        };
         let smoothed_iters = (iters) as f32 - (z.abs_sq().ln() * 0.5).ln() / ln_exponent;
 
         FractalResult {
@@ -74,20 +84,36 @@ pub(crate) trait Fractal: private::Modifier {
 mod private {
     use super::{Complex, FragmentConstants};
     pub trait Modifier {
+        /// Override as necessary
         #[inline(always)]
         fn modify(&self, _z: &mut Complex) {}
+
+        /// Override as necessary
         #[inline(always)]
         fn iterate_algorithm(
             &self,
             constants: &FragmentConstants,
             z: Complex,
             c: Complex,
+            iters: u32,
+        ) -> Complex {
+            self.iterate_algorithm_base(constants, z, c, iters)
+        }
+
+        /// Do not override!
+        #[inline(always)]
+        fn iterate_algorithm_base(
+            &self,
+            constants: &FragmentConstants,
+            z: Complex,
+            c: Complex,
             _iters: u32,
         ) -> Complex {
-            if constants.exponent_i == 2 {
-                z * z + c
-            } else {
-                z.powi(constants.exponent_i).to_rectangular() + c
+            use shader_common::NumericType;
+            match constants.exponent.typ {
+                NumericType::Integer if constants.exponent.int == 2 => z * z + c,
+                NumericType::Integer => z.powi(constants.exponent.int).to_rectangular() + c,
+                NumericType::Float => z.powf(constants.exponent.float).to_rectangular() + c,
             }
         }
     }
@@ -136,15 +162,11 @@ impl private::Modifier for Celtic {
         constants: &FragmentConstants,
         z: Complex,
         c: Complex,
-        _iters: u32,
+        iters: u32,
     ) -> Complex {
         // Based on mandelbrot, but using the formula:
         //   z := abs(re(z^2)) + i.im(z^2) + c
-        let zz = if constants.exponent_i == 2 {
-            z * z
-        } else {
-            z.powi(constants.exponent_i).to_rectangular()
-        };
+        let zz = self.iterate_algorithm_base(constants, z, c, iters);
         Complex {
             // unrolled version (fixed power):
             // re: (z.re * z.re - z.im * z.im).abs() + c.re,
@@ -174,12 +196,7 @@ impl private::Modifier for Variant {
         c: Complex,
         iters: u32,
     ) -> Complex {
-        // Based on mandelbrot, but take abs(Re(z)) on odd iterations
-        let zz = if constants.exponent_i == 2 {
-            z * z
-        } else {
-            z.powi(constants.exponent_i).to_rectangular()
-        };
+        let zz = self.iterate_algorithm_base(constants, z, c, iters);
         if (iters % 2) == 1 {
             Complex {
                 re: zz.re.abs() + c.re,

shader/src/lib.rs
@@ -141,7 +141,7 @@ fn log_rainbow(t: f32) -> Vec3 {
 mod tests {
     use super::{FragmentConstants, GRID_SIZE, RenderData};
 
-    use shader_common::Algorithm;
+    use shader_common::{Algorithm, PushExponent};
     use shader_util::Size;
     use spirv_std::glam::{Vec4, vec2, vec4};
 
@@ -170,7 +170,7 @@ mod tests {
             max_iter: 10,
             needs_reiterate: true.into(),
             algorithm: Algorithm::Mandelbrot,
-            exponent_i: 2,
+            exponent: PushExponent::from(2),
         }
     }
 

shader_common/src/lib.rs
@@ -21,7 +21,7 @@ pub struct FragmentConstants {
     pub algorithm: Algorithm,
     pub max_iter: u32,
     pub needs_reiterate: Bool,
-    pub exponent_i: i32,
+    pub exponent: PushExponent,
 }
 
 #[derive(Copy, Clone, Debug, Default)]
@@ -49,6 +49,34 @@ impl RenderData {
                 iters,
                 smooth_iters,
             }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default, PartialEq)]
+#[cfg_attr(not(target_arch = "spirv"), derive(NoUninit))]
+#[repr(u32)]
+pub enum NumericType {
+    #[default]
+    Integer,
+    Float,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+#[cfg_attr(not(target_arch = "spirv"), derive(NoUninit))]
+#[repr(C)]
+pub struct PushExponent {
+    pub typ: NumericType,
+    pub int: i32,
+    pub float: f32,
+}
+
+impl From<i32> for PushExponent {
+    fn from(i: i32) -> Self {
+        Self {
+            typ: NumericType::Integer,
+            int: i,
+            float: 0.,
         }
     }
 }

ui3/src/controller/keyboard.rs
@@ -74,14 +74,16 @@ impl super::Controller {
 
     fn expo(&mut self, increase: bool, active: bool) {
         if active {
-            self.movement.exponent = if increase { 1 } else { -1 };
+            let magnitude = self.exponent.step();
+            let sign = if increase { 1. } else { -1. };
+            self.movement.exponent = sign * magnitude;
         } else if increase {
-            if self.movement.exponent > 0 {
-                self.movement.exponent = 0;
+            if self.movement.exponent > 0. {
+                self.movement.exponent = 0.;
             }
-        } else if self.movement.exponent < 0 {
+        } else if self.movement.exponent < 0. {
             /* !inwards */
-            self.movement.exponent = 0;
+            self.movement.exponent = 0.;
         }
     }
 }

ui3/src/controller/mod.rs
@@ -27,8 +27,7 @@ pub(crate) struct Controller {
     vsync: bool,
     dragging: bool,
     algorithm: Algorithm,
-    exponent_u: u32,
-    exponent_negative: bool,
+    exponent: Exponent,
 }
 
 impl Controller {
@@ -48,8 +47,60 @@ impl Controller {
             vsync: true,
             dragging: false,
             algorithm: Algorithm::default(),
-            exponent_u: 2,
-            exponent_negative: false,
+            exponent: Exponent::default(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Exponent {
+    pub(crate) value: f32,
+    pub(crate) value_i: u32,
+    pub(crate) is_integer: bool,
+    pub(crate) is_negative: bool,
+}
+impl Default for Exponent {
+    fn default() -> Self {
+        Self {
+            value: 2.0,
+            value_i: 2,
+            is_integer: true,
+            is_negative: false,
+        }
+    }
+}
+impl Exponent {
+    fn variant(&self) -> NumericType {
+        if self.is_integer {
+            NumericType::Integer
+        } else {
+            NumericType::Float
+        }
+    }
+    fn as_int(&self) -> i32 {
+        self.value as i32 * if self.is_negative { -1 } else { 1 }
+    }
+    fn as_float(&self) -> f32 {
+        self.value * if self.is_negative { -1. } else { 1. }
+    }
+    fn step(&self) -> f32 {
+        if self.is_integer { 1. } else { 0.1 }
+    }
+}
+impl From<Exponent> for PushExponent {
+    fn from(exp: Exponent) -> Self {
+        if exp.is_integer {
+            PushExponent {
+                typ: NumericType::Integer,
+                int: exp.as_int(),
+                float: 0.,
+            }
+        } else {
+            PushExponent {
+                typ: NumericType::Float,
+                int: 0,
+                float: exp.as_float(),
+            }
         }
     }
 }
@@ -58,7 +109,7 @@ impl Controller {
 struct Movement {
     translate: DVec2,
     zoom: f64,
-    exponent: i32,
+    exponent: f32,
 }
 
 impl ControllerTrait for Controller {
@@ -81,7 +132,7 @@ impl ControllerTrait for Controller {
             algorithm: self.algorithm,
             max_iter: self.max_iter,
             needs_reiterate: reiterate.into(),
-            exponent_i: self.exponent_u as i32 * if self.exponent_negative { -1 } else { 1 },
+            exponent: self.exponent.into(),
         }
     }
 

ui3/src/controller/ui.rs
@@ -5,6 +5,11 @@ use shader_common::Algorithm;
 
 const DEFAULT_WIDTH: f32 = 130.;
 
+const EXPONENT_MIN: f32 = 0.;
+const EXPONENT_MIN_INT: u32 = 0;
+const EXPONENT_MAX: f32 = 20.;
+const EXPONENT_MAX_INT: u32 = 20;
+
 impl super::Controller {
     pub(super) fn ui_impl(
         &mut self,
@@ -37,13 +42,17 @@ impl super::Controller {
             self.viewport_translate += movement.translate / self.viewport_zoom * dt;
             self.reiterate = true;
         }
-        if movement.exponent != 0 {
-            let new_exponent = (self.exponent_u as i32 + movement.exponent).clamp(2, 30);
-            if new_exponent != self.exponent_u as i32 {
-                self.exponent_u = new_exponent as u32;
+        if movement.exponent != 0. {
+            let new_exp =
+                (self.exponent.value + movement.exponent).clamp(EXPONENT_MIN, EXPONENT_MAX);
+            if self.exponent.value != new_exp {
                 self.reiterate = true;
+                self.exponent.value = new_exp;
+                if self.exponent.is_integer {
+                    self.exponent.value_i = self.exponent.value.round() as u32;
+                }
             }
-            movement.exponent = 0;
+            movement.exponent = 0.;
         }
     }
 
@@ -52,6 +61,8 @@ impl super::Controller {
             .default_width(DEFAULT_WIDTH)
             .resizable(false)
             .show(ctx, |ui| {
+                use shader_common::NumericType;
+
                 let algorithm_before = self.algorithm;
                 egui::ComboBox::from_label(egui::RichText::new("Algorithm").size(15.0))
                     .selected_text(format!("{:?}", self.algorithm))
@@ -66,15 +77,60 @@ impl super::Controller {
                     self.reiterate = true;
                 }
                 ui.separator();
-                ui.label(egui::RichText::new("Exponent"));
-                if ui
-                    .add(egui::Slider::new(&mut self.exponent_u, 2..=30))
-                    .changed()
-                {
-                    self.reiterate = true;
+                ui.vertical_centered(|ui| {
+                    ui.label(egui::RichText::new("Exponent").size(15.0));
+                });
+                egui::Grid::new("exponent_grid").show(ui, |ui| {
+                    let previous_int = self.exponent.is_integer;
+                    ui.radio_value(&mut self.exponent.is_integer, true, "Integer");
+                    ui.radio_value(&mut self.exponent.is_integer, false, "Float");
+                    ui.end_row();
+                    if self.exponent.is_integer != previous_int {
+                        if previous_int {
+                            // was integer, now float
+                            self.exponent.value = self.exponent.value_i as f32;
+                        } else {
+                            // was float, now integer
+                            self.exponent.value_i = self.exponent.value.round() as u32;
+                            self.exponent.value = self.exponent.value_i as f32;
+                        }
+                        self.reiterate = true;
+                    }
+                });
+                match self.exponent.variant() {
+                    NumericType::Integer => {
+                        if ui
+                            .add(egui::Slider::new(
+                                &mut self.exponent.value_i,
+                                EXPONENT_MIN_INT..=EXPONENT_MAX_INT,
+                            ))
+                            .changed()
+                        {
+                            self.exponent.value = self.exponent.value_i as f32;
+                            self.reiterate = true;
+                        }
+                    }
+                    NumericType::Float => {
+                        if ui
+                            .add(
+                                egui::Slider::new(
+                                    &mut self.exponent.value,
+                                    EXPONENT_MIN..=EXPONENT_MAX,
+                                )
+                                .step_by(0.1),
+                            )
+                            .changed()
+                        {
+                            self.reiterate = true;
+                        }
+                    }
                 }
+
                 if ui
-                    .add(egui::Checkbox::new(&mut self.exponent_negative, "Negative"))
+                    .add(egui::Checkbox::new(
+                        &mut self.exponent.is_negative,
+                        "Negative",
+                    ))
                     .changed()
                 {
                     self.reiterate = true;

commit 0541c2a9a4cc3962f2938051cdbeff11bb6843ab

fix: celtic
This was broken by commit 9a47d480cfb4. Fixing forwards.

shader/src/fractal.rs
@@ -167,14 +167,12 @@ impl<E: Exponentiator> private::Modifier<E> for Celtic<E> {
     fn iterate_algorithm(&self, e: E, z: Complex, c: Complex, _iters: u32) -> Complex {
         // Based on mandelbrot, but using the formula:
         //   z := abs(re(z^2)) + i.im(z^2) + c
-        let zz = e.apply_to(z) + c;
-        Complex {
-            // unrolled version (fixed power):
-            // re: (z.re * z.re - z.im * z.im).abs() + c.re,
-            // im: 2.0 * z.re * z.im + c.im,
-            re: zz.re.abs() + c.re,
-            im: zz.im + c.im,
-        }
+        let zz = e.apply_to(z);
+        let z2 = Complex {
+            re: zz.re.abs(),
+            im: zz.im,
+        };
+        z2 + c
     }
 }
 