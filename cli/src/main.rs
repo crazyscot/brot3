5030640fc4953a811859c44034813cb5bc87dbbf
Ross Younger <crazyscot@gmail.com>
2025-10-12 22:51:36 +1300

    chore: remove old engine & cli directories

--- cli/src/main.rs
+++
@@ -1,6 +0,0 @@
-//! brot3 command line interface
-// (c) 2024 Ross Younger
-
-fn main() -> anyhow::Result<()> {
-    brot3_cli::cli_main()
-}
--- engine/src/render/mod.rs
+++
@@ -1,8 +0,0 @@
-// Conversion of fractal PointData into its output format
-// (c) 2024 Ross Younger
-mod ascii;
-mod framework;
-mod png;
-
-pub use framework::{IRenderer, Renderer};
-pub use png::Png;
