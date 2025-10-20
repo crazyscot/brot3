a83ae8d9aee0cf8d24d8f6187acd10e00e28bfc7
Ross Younger <crazyscot@gmail.com>
2025-10-20 17:08:48 +1300

    chore: handle shader build failures more tidily

--- shader_builder/src/main.rs
+++ shader_builder/src/main.rs
@@ -2,20 +2,21 @@ use spirv_builder::SpirvBuilder;
 use std::env;
 use std::path::Path;
 
-fn build_shader(path_to_crate: &str) {
+fn build_shader(path_to_crate: &str) -> anyhow::Result<()> {
     let builder_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let path_to_crate = builder_dir.join(path_to_crate);
     let mut builder = SpirvBuilder::new(path_to_crate, "spirv-unknown-vulkan1.1");
     if env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default() == "wasm32" {
         builder = builder.shader_crate_features(["emulate_constants".into()]);
     }
-    let compile_result = builder.build().unwrap();
+    let compile_result = builder.build()?;
     let shader_path = std::fs::canonicalize(compile_result.module.unwrap_single()).unwrap();
     let file_name = shader_path.file_name().unwrap().to_str().unwrap();
     println!("cargo::rustc-env={}={}", file_name, shader_path.display());
+    Ok(())
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // CAUTION: Hard-wired path !
-    build_shader("../shader");
+    build_shader("../shader")
 }
