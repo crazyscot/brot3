323fbbb4b95b939ebf3c87df412cdb5dd9d32626
Ross Younger <crazyscot@gmail.com>
2026-03-08 23:13:16 +1300

    tidyup: drop now-empty util crate, update ci

--- util/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Utility and data wrangling code used by the UI and shader crates.
-//!
-//! This is a separate crate for efficiency of testing.
-
-#![cfg_attr(target_arch = "spirv", no_std)]
-#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
