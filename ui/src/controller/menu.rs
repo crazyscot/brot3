ebe95a460c59b50078929f50a74a3776317ff062
Ross Younger <crazyscot@gmail.com>
2026-03-11 22:19:03 +1300

    style: move debug options into a submenu

--- ui/src/controller/menu.rs
+++ ui/src/controller/menu.rs
@@ -5,6 +5,26 @@ use easy_shader_runner::egui::{self, vec2};
 
 use crate::widgets::CheckableButton;
 
+macro_rules! checkbox {
+    ($ui:expr, $var:expr, $lbl:literal) => {
+        CheckableButton::new(&mut $var, $lbl, || false)
+            .min_size(vec2(ITEM_WIDTH, 0.0))
+            .ui($ui)
+    };
+    ($ui:expr, $var:expr, $lbl:literal, $accel:expr) => {
+        CheckableButton::new(&mut $var, $lbl, || false)
+            .shortcut_text($accel)
+            .min_size(vec2(ITEM_WIDTH, 0.0))
+            .ui($ui)
+    };
+    ($ui:expr, $var:expr, $lbl:literal, $accel:expr, $indet: expr) => {
+        CheckableButton::new(&mut $var, $lbl, $indet)
+            .shortcut_text($accel)
+            .min_size(vec2(ITEM_WIDTH, 0.0))
+            .ui($ui)
+    };
+}
+
 #[allow(unused_results)]
 impl super::Controller {
     pub(crate) fn main_menu(&mut self, ctx: &egui::Context) {
@@ -17,25 +37,6 @@ impl super::Controller {
                     const ITEM_WIDTH: f32 = 120.0;
                     use egui::Widget as _;
 
-                    macro_rules! checkbox {
-                        ($var:expr, $lbl:literal) => {
-                            CheckableButton::new(&mut $var, $lbl, || false)
-                                .min_size(vec2(ITEM_WIDTH, 0.0))
-                                .ui(ui)
-                        };
-                        ($var:expr, $lbl:literal, $accel:expr) => {
-                            CheckableButton::new(&mut $var, $lbl, || false)
-                                .shortcut_text($accel)
-                                .min_size(vec2(ITEM_WIDTH, 0.0))
-                                .ui(ui)
-                        };
-                        ($var:expr, $lbl:literal, $accel:expr, $indet: expr) => {
-                            CheckableButton::new(&mut $var, $lbl, $indet)
-                                .shortcut_text($accel)
-                                .min_size(vec2(ITEM_WIDTH, 0.0))
-                                .ui(ui)
-                        };
-                    }
                     macro_rules! item {
                         ($label:expr, $accel:expr) => {
                             egui::Button::new($label)
@@ -43,13 +44,14 @@ impl super::Controller {
                                 .min_size(vec2(ITEM_WIDTH, 0.0))
                         };
                     }
-                    checkbox!(self.show_controls, "Controls", "F2");
-                    checkbox!(self.show_coords_window, "Data read-out", "F3");
-                    checkbox!(self.show_scale_bar, "Scale bar", "F4");
+                    checkbox!(ui, self.show_controls, "Controls", "F2");
+                    checkbox!(ui, self.show_coords_window, "Data read-out", "F3");
+                    checkbox!(ui, self.show_scale_bar, "Scale bar", "F4");
 
                     // Fullscreen is tricky. On OSX the OS may change the state; we are not the sole
                     // arbiters.
                     if checkbox!(
+                        ui,
                         self.fullscreen_checkbox,
                         "Fullscreen",
                         if cfg!(target_os = "macos") {
@@ -66,24 +68,28 @@ impl super::Controller {
 
                     ui.separator();
 
-                    checkbox!(self.show_fps, "Show FPS");
-                    checkbox!(self.vsync, "vsync");
-                    checkbox!(self.always_reiterate, "Always reiterate");
-                    ui.separator();
-                    if checkbox!(
-                        self.perturbation_mode,
-                        "Force perturbation mode",
-                        "",
-                        || !self.force_perturb
-                    )
-                    .clicked()
-                    {
-                        self.force_perturb = true;
-                        self.reiterate = true;
-                    }
+                    ui.menu_button("Debug", |ui| {
+                        checkbox!(ui, self.show_fps, "Show FPS");
+
+                        checkbox!(ui, self.vsync, "vsync");
+                        checkbox!(ui, self.always_reiterate, "Always reiterate");
+                        ui.separator();
+                        if checkbox!(
+                            ui,
+                            self.perturbation_mode,
+                            "Force perturbation mode",
+                            "",
+                            || !self.force_perturb
+                        )
+                        .clicked()
+                        {
+                            self.force_perturb = true;
+                            self.reiterate = true;
+                        }
+                    });
 
                     ui.separator();
-                    checkbox!(self.keyboard_help, "Show Help", "F1");
+                    checkbox!(ui, self.keyboard_help, "Show Help", "F1");
 
                     if ui.add(item!("About", "")).clicked() {
                         self.show_about = true;
