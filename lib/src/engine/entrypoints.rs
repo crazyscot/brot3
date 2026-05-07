14acc81fe64d62478329ea4b7b9e3de0dcf6a716
Ross Younger <crazyscot@gmail.com>
2026-05-07 20:47:17 +1200

    refactor: move the SPIR-V entrypoints into engine::entrypoints

--- lib/src/engine/entrypoints.rs
+++ lib/src/engine/entrypoints.rs
@@ -2,16 +2,122 @@
 //!
 //! (c) 2025-6 Ross Younger, with inspiration from earlier work by Abel <abel465@gmail.com>; see <https://github.com/abel465/mandelbrot>
 
+use easy_cast::Cast as _;
+#[allow(unused_imports)] // Some are reused in some configurations
+use spirv_std::spirv;
+
+use crate::{
+    INSPECTOR_MARKER_SIZE, UVec3, Vec2, Vec3Swizzles as _, Vec4, Vec4Swizzles as _,
+    data::{Flags, FragmentConstants, PointResult},
+    engine,
+    util::{GridRef, GridRefMut, GridShared, PackedRgba8, RgbVec},
+    vec2,
+};
+
+/// SPIRV `fragment` entrypoint.
+/// This does the iteration and rendering work.
+pub(crate) fn main_fs(
+    frag_coord: Vec4,
+    #[cfg(not(feature = "emulate_constants"))] constants: &FragmentConstants,
+    #[cfg(feature = "emulate_constants")] constants: &FragmentConstants,
+    grid: &mut [PointResult],
+    perturbation_reference_points: &[Vec2],
+    output: &mut Vec4,
+) {
+    // window-relative coords (0,W) x (0,H) (they might be half pixels e.g. 0.5 to 1023.5); we
+    // ignore depth & 1/w
+    let coord = frag_coord.xy();
+    let coord_int = coord.as_uvec2();
+
+    // viewport pixel size e.g. 1920x1080
+    let size = constants.size.as_vec2();
+    let pixel_spacing = constants.pixel_spacing();
+
+    // convert pixel coordinates to complex units such that (0,0) is at the centre of the viewport
+    let complex_offset = (coord - 0.5 * size) * pixel_spacing;
+
+    let cache = GridRef::new(constants.buffer_size.as_uvec2(), grid);
+    let cacheable = cache.address_valid(coord_int);
+
+    let render_data = if !cacheable {
+        engine::render(constants, complex_offset, perturbation_reference_points)
+    } else if constants.flags.contains(Flags::NEEDS_REITERATE) {
+        let render_data = engine::render(constants, complex_offset, perturbation_reference_points);
+        let mut cache = GridRefMut::new(constants.buffer_size.as_uvec2(), grid);
+        cache.set(coord_int, render_data);
+        render_data
+    } else {
+        // it's cacheable and cached
+        cache.get(coord_int)
+    };
+
+    let mut colour = engine::colour_data(render_data, constants, pixel_spacing);
+
+    // Draw the inspector marker
+    if constants.flags.contains(Flags::INSPECTOR_ACTIVE) {
+        // New York distance from the reference point draws a diamond shape
+        let dist = engine::new_york_distance(constants.inspector_point_pixel_address, coord);
+        if dist < INSPECTOR_MARKER_SIZE * 0.667 {
+            // TODO Do something better here? Change pixels underneath?
+            colour = RgbVec::BLACK;
+        } else if dist < INSPECTOR_MARKER_SIZE {
+            colour = RgbVec::WHITE;
+        }
+    }
+
+    *output = colour.0.extend(1.0);
+}
+
+/// SPIRV `vertex` entrypoint.
+pub(crate) fn main_vs(vert_id: i32, out_pos: &mut Vec4) {
+    let uv = vec2(((vert_id << 1) & 2).cast(), (vert_id & 2).cast());
+    // uv expresses the cycle: (0,0) (2,0) (0,2) (2,2)
+    let pos = 2.0 * uv - Vec2::ONE;
+    // pos expresses the cycle: (-1,-1) (3,-1) (-1,3) (3,3)
+
+    *out_pos = pos.extend(0.0).extend(1.0);
+}
+
+/// SPIRV compute shader entrypoint
+pub(crate) fn main_cs(
+    gid: UVec3,
+    #[cfg(not(feature = "emulate_constants"))] constants: &FragmentConstants,
+    #[cfg(feature = "emulate_constants")] constants: &FragmentConstants,
+    pixels: &mut [u32],
+    perturbation_reference_points: &[Vec2],
+) {
+    // Input gid is the pixel address
+    let coord_int = gid.xy();
+    let coord = coord_int.as_vec2();
+
+    // viewport pixel size e.g. 1920x1080
+    let size = constants.size.as_vec2();
+    let pixel_spacing = constants.pixel_spacing();
+
+    // convert pixel coordinates to complex units such that (0,0) is at the centre of the viewport
+    let complex_offset = (coord - 0.5 * size) * pixel_spacing;
+
+    let render_data = engine::render(constants, complex_offset, perturbation_reference_points);
+    let colour = engine::colour_data(render_data, constants, pixel_spacing);
+
+    // no inspector marker in compute shader
+
+    let mut matrix = GridRefMut::new(constants.size.as_uvec2(), pixels);
+    matrix.set(gid.xy(), PackedRgba8::from(colour).0);
+}
+
 #[cfg(test)]
 #[cfg_attr(coverage_nightly, coverage(off))]
 mod tests {
     use float_eq::assert_float_eq;
-    use spirv_std::glam::{UVec2, Vec2, Vec3, Vec4, uvec2, vec2, vec4};
 
+    use super::{PackedRgba8, RgbVec};
     use crate::{
+        UVec2, Vec2, Vec3, Vec4,
         data::{Algorithm, Colourer, Flags, FragmentConstants, Palette, PointResult, PushExponent},
         engine::new_york_distance,
         util::Size,
+        uvec2, uvec3, vec2, vec3, vec4,
     };
 
     const TEST_GRID_SIZE: UVec2 = uvec2(2560, 1440);
@@ -164,5 +270,23 @@ mod tests {
                 ulps <= 4
             );
         }
+    }
+
+    #[test]
+    fn compute_shader() {
+        let mut pixels = vec![0u32; (TEST_GRID_SIZE.x * TEST_GRID_SIZE.y) as usize];
+        let perturbation_reference_points = vec![];
+
+        let constants = test_frag_consts();
+
+        crate::main_cs(
+            uvec3(0, 0, 0),
+            &constants,
+            &mut pixels,
+            &perturbation_reference_points,
+        );
+
+        let expected_colour = PackedRgba8::from(RgbVec::from(vec3(0.0, 1.0, 0.141_448_5))).0;
+        assert_eq!(pixels[0], expected_colour);
     }
 }
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -13,10 +13,11 @@
 #![allow(missing_docs)]
 
 pub use easy_cast;
-use easy_cast::Cast as _;
 /// Local glam re-exports for convenience
+#[allow(unused_imports)] // Some are reused in tests
 pub(crate) use spirv_std::glam::{
-    UVec2, UVec3, Vec2, Vec3, Vec3Swizzles as _, Vec4, f32, uvec2, uvec3, vec2, vec3,
+    UVec2, UVec3, Vec2, Vec3, Vec3Swizzles, Vec4, Vec4Swizzles, f32, uvec2, uvec3, uvec4, vec2,
+    vec3, vec4,
 };
 #[cfg(spirv)]
 use spirv_std::num_traits::real::Real;
@@ -35,12 +36,8 @@ pub use bignum::{
     big_complex::{BigComplex, ParseError as BigComplexParseError},
     big_vec2::{BigVec2, fbig_from_str},
 };
-use spirv_std::glam::Vec4Swizzles as _;
 
-use crate::{
-    data::{Flags, FragmentConstants, PointResult},
-    util::{GridRef, GridRefMut, GridShared, PackedRgba8, RgbVec},
-};
+use crate::data::{FragmentConstants, PointResult};
 
 /// Complex type used throughout shader
 pub type Complex = abels_complex::Complex<f32>;
@@ -57,6 +54,7 @@ const ESCAPE_THRESHOLD_SQ: f32 = ESCAPE_THRESHOLD * ESCAPE_THRESHOLD;
 #[spirv(fragment)]
 pub fn main_fs(
     #[spirv(frag_coord)] frag_coord: Vec4,
+
     #[cfg(not(feature = "emulate_constants"))]
     #[spirv(push_constant)]
     constants: &FragmentConstants,
@@ -69,48 +67,13 @@ pub fn main_fs(
     perturbation_reference_points: &[Vec2],
     output: &mut Vec4,
 ) {
-    // window-relative coords (0,W) x (0,H) (they might be half pixels e.g. 0.5 to 1023.5); we
-    // ignore depth & 1/w
-    let coord = frag_coord.xy();
-    let coord_int = coord.as_uvec2();
-
-    // viewport pixel size e.g. 1920x1080
-    let size = constants.size.as_vec2();
-    let pixel_spacing = constants.pixel_spacing();
-
-    // convert pixel coordinates to complex units such that (0,0) is at the centre of the viewport
-    let complex_offset = (coord - 0.5 * size) * pixel_spacing;
-
-    let cache = GridRef::new(constants.buffer_size.as_uvec2(), grid);
-    let cacheable = cache.address_valid(coord_int);
-
-    let render_data = if !cacheable {
-        engine::render(constants, complex_offset, perturbation_reference_points)
-    } else if constants.flags.contains(Flags::NEEDS_REITERATE) {
-        let render_data = engine::render(constants, complex_offset, perturbation_reference_points);
-        let mut cache = GridRefMut::new(constants.buffer_size.as_uvec2(), grid);
-        cache.set(coord_int, render_data);
-        render_data
-    } else {
-        // it's cacheable and cached
-        cache.get(coord_int)
-    };
-
-    let mut colour = engine::colour_data(render_data, constants, pixel_spacing);
-
-    // Draw the inspector marker
-    if constants.flags.contains(Flags::INSPECTOR_ACTIVE) {
-        // New York distance from the reference point draws a diamond shape
-        let dist = engine::new_york_distance(constants.inspector_point_pixel_address, coord);
-        if dist < INSPECTOR_MARKER_SIZE * 0.667 {
-            // TODO Do something better here? Change pixels underneath?
-            colour = RgbVec::BLACK;
-        } else if dist < INSPECTOR_MARKER_SIZE {
-            colour = RgbVec::WHITE;
-        }
-    }
-
-    *output = colour.0.extend(1.0);
+    engine::entrypoints::main_fs(
+        frag_coord,
+        constants,
+        grid,
+        perturbation_reference_points,
+        output,
+    );
 }
 
 /// SPIRV `vertex` entrypoint.
@@ -119,12 +82,7 @@ pub fn main_vs(
     #[spirv(vertex_index)] vert_id: i32,
     #[spirv(position, invariant)] out_pos: &mut Vec4,
 ) {
-    let uv = vec2(((vert_id << 1) & 2).cast(), (vert_id & 2).cast());
-    // uv expresses the cycle: (0,0) (2,0) (0,2) (2,2)
-    let pos = 2.0 * uv - Vec2::ONE;
-    // pos expresses the cycle: (-1,-1) (3,-1) (-1,3) (3,3)
-
-    *out_pos = pos.extend(0.0).extend(1.0);
+    engine::entrypoints::main_vs(vert_id, out_pos);
 }
 
 pub const COMPUTE_SHADER_THREADS: UVec3 = uvec3(64, 1, 1);
@@ -133,6 +91,7 @@ pub const COMPUTE_SHADER_THREADS: UVec3 = uvec3(64, 1, 1);
 #[spirv(compute(threads(64, 1, 1)))]
 pub fn main_cs(
     #[spirv(global_invocation_id)] gid: UVec3,
+
     #[cfg(not(feature = "emulate_constants"))]
     #[spirv(push_constant)]
     constants: &FragmentConstants,
@@ -144,22 +103,5 @@ pub fn main_cs(
     #[spirv(storage_buffer, descriptor_set = 0, binding = 1)]
     perturbation_reference_points: &[Vec2],
 ) {
-    // Input gid is the pixel address
-    let coord_int = gid.xy();
-    let coord = coord_int.as_vec2();
-
-    // viewport pixel size e.g. 1920x1080
-    let size = constants.size.as_vec2();
-    let pixel_spacing = constants.pixel_spacing();
-
-    // convert pixel coordinates to complex units such that (0,0) is at the centre of the viewport
-    let complex_offset = (coord - 0.5 * size) * pixel_spacing;
-
-    let render_data = engine::render(constants, complex_offset, perturbation_reference_points);
-    let colour = engine::colour_data(render_data, constants, pixel_spacing);
-
-    // no inspector marker in compute shader
-
-    let mut matrix = GridRefMut::new(constants.size.as_uvec2(), pixels);
-    matrix.set(gid.xy(), PackedRgba8::from(colour).0);
+    engine::entrypoints::main_cs(gid, constants, pixels, perturbation_reference_points);
 }
