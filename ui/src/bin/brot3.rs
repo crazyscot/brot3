cfaf00aabaa173f96506edcc23496cc42e08b3b6
Ross Younger <crazyscot@gmail.com>
2026-05-12 22:18:06 +1200

    fix(windows): When run on the command line, attach to that command line.

--- ui/src/bin/brot3.rs
+++ ui/src/bin/brot3.rs
@@ -9,6 +9,13 @@ static ALLOCATOR: mimalloc::MiMalloc = mimalloc::MiMalloc;
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
+    #[cfg(target_os = "windows")]
+    #[allow(unsafe_code)]
+    {
+        use windows::Win32::System::Console::{ATTACH_PARENT_PROCESS, AttachConsole};
+        let _ = unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
+    }
+
     if let Err(e) = brot3_ui::main() {
         eprintln!("{e}");
         return ExitCode::FAILURE;
