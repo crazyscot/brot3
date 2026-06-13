15029569ac7af3b5e6f68abdbec8ea25b8b845aa
Ross Younger <crazyscot@gmail.com>
2026-06-13 21:57:41 +1200

    misc: When Show Timings is selected, show the FPS window if it's not already hidden

--- ui/src/controller/menu.rs
+++ ui/src/controller/menu.rs
@@ -80,7 +80,11 @@ impl super::Controller {
                     ui.menu_button("Debug", |ui| {
                         checkbox!(ui, self.show_fps, "Show FPS", "F5");
                         checkbox!(ui, self.show_shader, "Show selected shader");
-                        checkbox!(ui, self.show_timings, "Show timings");
+                        if checkbox!(ui, self.show_timings, "Show timings").clicked()
+                            && self.show_timings
+                        {
+                            self.show_fps = true;
+                        }
 
                         ui.separator();
                         checkbox!(ui, self.vsync, "vsync");
